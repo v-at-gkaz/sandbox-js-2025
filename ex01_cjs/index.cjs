357d70e47151a53d3ff8caa651bd84158011cdca
Vladimir <v@gkaz.ru>
2025-09-27 10:55:03 +0300

    Upd: ex01 and ex02

--- ex01_cjs/index.cjs
+++ ex01_cjs/index.cjs
@@ -1,4 +1,42 @@
 const Square = require('./square.cjs');
 
 const mySquare = new Square(3);
-console.log(`The area of mySquare is ${mySquare.area()}`); 
+console.log(`The area of mySquare is ${mySquare.area()}`);
+
+function test() {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve('success');
+        }, 400);
+        setTimeout(()=>{
+            reject('error');
+        }, 500);
+    });
+}
+
+// async/await way
+(async () => {
+    try {
+        const result = await test();
+        console.log('result', result);
+    } catch(e){
+        console.error(`error detected: ${e}`);
+    }
+})();
+
+// scassical way
+// test().then(res=>{
+// console.log('success detected', res);
+// }).catch(err=>{
+// console.error(`error detected: ${err}`);
+// }).finally(()=>{
+// console.log('finaly detected');
+// });
+
+// wrong way (for cjs)
+// try {
+//     const result = await test();
+//     console.log('result', result);
+// } catch(e){
+//     console.error(`error detected: ${e}`);
+// }
--- ex02_mjs/index.mjs
+++ ex02_mjs/index.mjs
@@ -2,3 +2,23 @@ import Square from "./square.mjs";
 
 const mySquare = new Square(3);
 console.log(`The area of mySquare is ${mySquare.area()}`); 
+
+
+function test() {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve('success');
+        }, 400);
+        setTimeout(()=>{
+            reject('error');
+        }, 500);
+    });
+}
+
+// OK (mjs)
+try {
+    const result = await test();
+    console.log('result', result);
+} catch(e){
+    console.error(`error detected: ${e}`);
+}
