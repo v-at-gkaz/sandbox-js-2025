fff739da4cd7b09e649030a8a1975205efe601da
Vladimir <v@gkaz.ru>
2025-09-27 10:30:50 +0300

    Add ex01 and ex02

---
+++ ex01_cjs/index.cjs
@@ -0,0 +1,4 @@
+const Square = require('./square.cjs');
+
+const mySquare = new Square(3);
+console.log(`The area of mySquare is ${mySquare.area()}`); 
---
+++ ex02_mjs/index.mjs
@@ -0,0 +1,4 @@
+import Square from "./square.mjs";
+
+const mySquare = new Square(3);
+console.log(`The area of mySquare is ${mySquare.area()}`); 
